ab3e4f608dfb0b8f618e757b18a3c48a61182a96
JaeKyung Sung <jaekyungsung@woodstock.ac.in>
2020-05-22 12:42:03 +0900

    1. Create function for getting next open row

--- non_graphic_version/non_graphic_main.py
+++ non_graphic_version/non_graphic_main.py
@@ -1,23 +1,46 @@
 import numpy as np
 
+# global and constant
+ROW_COUNT = 6
+COL_COUNT = 7
 
+# Draw board
 def create_board():
     # Zero matrics 6 rows by 7 column
     board = np.zeros((6, 7))
     return board
 
 
+def drop_piece():
+    pass
+
+
+# Check if player input is valid
+def is_valid_location(board, column):
+    pass
+
+
+# Check for open row
+def get_next_open_row(board, column):
+    for row_num in range(ROW_COUNT):
+        if board[row_num][col]:
+            return row_num
+
+
+
+
 game_over = False
 turn = 0
 
 while not game_over:
+    print(create_board())
     # Ask player1 input
     if turn == 0:
-        selection_1 = int(input("PLayer1 Make your selection (0-6): "))
+        col_select_1 = int(input("PLayer1 Make your selection (0-6): "))
 
     # Ask player2 input
     else:
-        selection_1 = int(input("Player2 Make your selection (0-6): "))
+        col_select_2 = int(input("Player2 Make your selection (0-6): "))
 
     turn += 1
 
