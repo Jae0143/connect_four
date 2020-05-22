2831c15e8621092d9bd687516c4c21c037c63d52
JaeKyung Sung <jaekyungsung@woodstock.ac.in>
2020-05-22 12:58:28 +0900

    1. Fliping the board over the 0th row so that game show correctly

--- non_graphic_version/non_graphic_main.py
+++ non_graphic_version/non_graphic_main.py
@@ -4,6 +4,7 @@ import numpy as np
 ROW_COUNT = 6
 COL_COUNT = 7
 
+
 # Draw board
 def create_board():
     # Zero matrics 6 rows by 7 column
@@ -11,38 +12,52 @@ def create_board():
     return board
 
 
-def drop_piece():
-    pass
+# dropping piece into the game
+def drop_piece(board, row, column, player_piece):
+    board[row][column] = player_piece
 
 
 # Check if player input is valid
 def is_valid_location(board, column):
-    pass
+    return board[5][column] == 0
 
 
 # Check for open row
 def get_next_open_row(board, column):
     for row_num in range(ROW_COUNT):
-        if board[row_num][col]:
+        if board[row_num][column] == 0:
             return row_num
 
 
+# flip the board so the top row goes to the bottom
+def print_board(board):
+    # flip board over 0th row
+    return np.flip(board, 0)
 
 
 game_over = False
 turn = 0
 
+board = create_board()
+
 while not game_over:
-    print(create_board())
     # Ask player1 input
     if turn == 0:
         col_select_1 = int(input("PLayer1 Make your selection (0-6): "))
+        if is_valid_location(board, col_select_1):
+            row = get_next_open_row(board, col_select_1)
+            drop_piece(board, row, col_select_1, 1)
 
     # Ask player2 input
     else:
         col_select_2 = int(input("Player2 Make your selection (0-6): "))
+        if is_valid_location(board, col_select_2):
+            row = get_next_open_row(board, col_select_2)
+            drop_piece(board, row, col_select_2, 2)
 
     turn += 1
 
     # divides by 2 -> giving option of 0 and 1
     turn = turn % 2
+
+    print(print_board(board))
